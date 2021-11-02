443d6f1badcfc32e0869d9af1a86be850f26311b
Euge Dallasta <mariaedallasta@gmail.com>
2021-11-02 18:35:00 +0100

    Preparing folder

---
+++ src/films.js
@@ -0,0 +1,41 @@
+// Exercise 1: Get the array of all directors.
+
+}
+
+// Exercise 2: Get the films of a certain director
+
+
+// Exercise 3: Calculate the average of the films of a given director.
+
+
+// Exercise 4:  Alphabetic order by title 
+
+// Exercise 5: Order by year, ascending
+
+
+// Exercise 6: Calculate the average of the movies in a category
+
+
+// Exercise 7: Modify the duration of movies to minutes
+
+
+// Exercise 8: Get the best film of a year
+
+
+
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = {
+    getAllDirectors,
+    getMoviesFromDirector,
+    moviesAverageOfDirector,
+    orderAlphabetically,
+    orderByYear,
+    moviesAverageByCategory,
+    hoursToMinutes,
+    bestFilmOfYear,
+  };
+}
